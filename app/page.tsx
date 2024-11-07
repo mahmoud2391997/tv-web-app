5e71381da4ad2de4fd5e02fd644b47432a4bb81e
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-11-07 09:38:17 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">
+        Welcome to My Next.js App with Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
